8c66cb32a6993645c9693e77779120b1cdc72a62
Doeun Kim <doeuning@gmail.com>
2019-10-18 18:47:57 +0900

    Updates

---
+++ precache-manifest.1e36efcc0bbab2aef61666d94faefdfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a50c0bb6a66e5c126a42ca52964c94a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3e9bc546871137dc5dc5",
+    "url": "/portfolio/static/css/main.0118958e.chunk.css"
+  },
+  {
+    "revision": "8fa56176ccf97b4232b7",
+    "url": "/portfolio/static/js/2.89bfb487.chunk.js"
+  },
+  {
+    "revision": "3e9bc546871137dc5dc5",
+    "url": "/portfolio/static/js/main.baa7d550.chunk.js"
+  },
+  {
+    "revision": "a3c97eef0867f0bfdd85",
+    "url": "/portfolio/static/js/runtime-main.0f53917c.js"
+  }
+]);
